2106c256900f3ca6a804abad9d8eb3cc48afb21e
David Mejia <davidndy@gmail.com>
2018-08-10 20:46:09 -0500

    Updating git

--- 02_difference_of_squares/problem/difference_of_squares.rb
+++ 02_difference_of_squares/problem/difference_of_squares.rb
@@ -27,3 +27,29 @@ x = 10
 
  p Squares.new(x).difference
 
+
+class Squares2
+attr_accessor :n
+
+  def initialize(n)
+    @n = n
+  end
+  
+  def difference
+    square_of_sum(@n)**2 - sum_of_squares(@n)
+  end
+  
+  def build_vector(x)
+    x <= 1 ? [x] : build_vector(x-1) << x
+  end
+
+  def square_of_sum(x)
+    x<=1 ? x : x+square_of_sum(x-1)
+  end
+
+  def sum_of_squares(x)
+    x<=1 ? x**2 : x**2+sum_of_squares(x-1)
+  end
+end
+
+p Squares2.new(x).difference
