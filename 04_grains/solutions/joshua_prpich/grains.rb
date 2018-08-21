d5184eeb5852a93e3dbe63c0518ea6cbc2585e31
Joshua <joshuaprpich@gmail.com>
2018-08-21 17:01:42 -0500

    refactor code for efficiency

--- 04_grains/solutions/joshua_prpich/grains.rb
+++ 04_grains/solutions/joshua_prpich/grains.rb
@@ -1,18 +1,14 @@
 class Grains
   def self.square(num)
     raise ArgumentError.new("num must be between 1 and 64") if num < 1 || num > 64
-    result = 1
-    2.upto(num).each do |x|
-      result *= 2
-    end
-    result
+    2 ** (num-1)
   end
 
   def self.total
     result = 1
-    2.upto(64) do |x|
-      result+=self.square(x)
+    64.times do
+      result *= 2
     end
-    result
+    result-1
   end
 end
